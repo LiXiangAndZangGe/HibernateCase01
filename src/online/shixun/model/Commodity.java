47dfe6837921d7698c3b5829e97122de2fa96776
yuanyixiong <15926499574@163.com>
2017-09-19 11:29:30 +0800

    优化规范

--- src/online/shixun/model/Commodity.java
+++ src/online/shixun/model/Commodity.java
@@ -20,7 +20,7 @@ public class Commodity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 主键自增
-	private Integer id;
+	private Long id;
 
 	private String name;
 
@@ -36,11 +36,11 @@ public class Commodity {
 	@Temporal(TemporalType.DATE)
 	private Date modifyDate;
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
